54865653a5990bade165d0598ceaf5ba3c154d5e
alejo4373 <alejo4373@gmail.com>
2017-11-02 00:14:17 -0400

    refactored so that the we only add the class id the element is a div

--- html/DOM/events2/quadrant_toggle/quadrant-toggle.js
+++ html/DOM/events2/quadrant_toggle/quadrant-toggle.js
@@ -1,5 +1,8 @@
-document.addEventListener('click', function(event){
-    console.log(event.target.id)
-    event.target.classList.add("black")
+document.addEventListener('click', function (event) {
+    console.log(event.target.classList)
+    if (event.target.nodeName === "DIV") {
+        let currentClass = event.target.classList
+        currentClass.contains("black") ? event.target.classList.remove("black")
+                                       : event.target.classList.add("black")
+    }
 })
- 
