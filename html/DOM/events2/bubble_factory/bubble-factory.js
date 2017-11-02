c5ea8fd49461fed700696dbdf54fa803c0cd5281
alejo4373 <alejo4373@gmail.com>
2017-11-02 11:14:48 -0400

    randomized position and size of bubbles

--- html/DOM/events2/bubble_factory/bubble-factory.js
+++ html/DOM/events2/bubble_factory/bubble-factory.js
@@ -1,5 +1,14 @@
-document.addEventListener('click', function(event){
-    console.log(event.target.id)
-    event.target.classList.add("black")
+document.addEventListener('DOMContentLoaded', function () {
+    document.querySelector('button').addEventListener('click', function (event) {
+        console.log(event.target);
+        let bubble = document.createElement('img')
+        bubble.src = "http://mzayat.com/images/bubble-20clipart-bubble-clip-art-256_251.png"
+        
+        bubble.style.width = ((Math.random() * 50) + 10) + "px";
+        bubble.style.position = "absolute";
+        bubble.style.top = ((Math.random() * 90) + 10) + "%";
+        bubble.style.left = ((Math.random() * 90) + 10) + "%";
+        
+        document.body.appendChild(bubble);
+    })
 })
- 
