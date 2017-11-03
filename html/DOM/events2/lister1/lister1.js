965cffa86554d71087b44b5dac5e1fe5dcc85ff4
alejo4373 <alejo4373@gmail.com>
2017-11-03 00:17:43 -0400

    added validation on input type=text

--- html/DOM/events2/lister1/lister1.js
+++ html/DOM/events2/lister1/lister1.js
@@ -1,5 +1,39 @@
-document.addEventListener('click', function(event){
-    console.log(event.target.id)
-    event.target.classList.add("black")
+//Prevent form submition
+document.querySelector("#myForm").addEventListener("submit", function (event) {
+    event.preventDefault();
 })
- 
+
+document.querySelector("#sendbutton").addEventListener("click", function () {
+    let content = document.querySelector("#inputtext").value;
+    if (content) {
+        let newItem = document.createElement("li");
+        let myList = document.querySelector("ul");  //Node to the <ul> element
+        newItem.innerText = content;
+        myList.appendChild(newItem);
+        document.querySelector("#inputtext").value = "";
+    }
+
+})
+
+document.addEventListener('click', function (event) {
+    if (event.target.nodeName === "LI") {
+        toggleDone(event.target);
+    }
+});
+
+document.querySelector("#removedone").addEventListener("click", function (event) {
+    let myList = document.querySelectorAll("li")
+    myList.forEach(function (currentElement) {
+        if (currentElement.style.textDecoration) {
+            currentElement.remove();
+        }
+    })
+})
+
+function toggleDone(htmlElement) {
+    if (!htmlElement.style.textDecoration) {
+        htmlElement.style.textDecoration = "line-through"
+    } else {
+        htmlElement.style.textDecoration = ""
+    }
+}
